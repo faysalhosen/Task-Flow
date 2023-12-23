a541fddf94b93dd4ecfc97e6254c92af7443f572
faysalhosen <faysanhosen7025@gmail.com>
2023-12-23 11:47:25 +0600

    error page added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,17 @@
-
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <div>
-            
+          <div className=" flex justify-center text-center mt-10 lg:mt-[10%]">
+           <img src="https://i.postimg.cc/Bn1b8f4D/Word-Press-support-UK.webp"  className ="rounded-lg w-96" alt="" />
+           
+         </div>
+        <div className="flex justify-center mt-5">
+        <Link to="/"><button className="btn btn-outline flex justify-center bg-red-500 px-2 py-2 rounded-lg font-semibold text-white">Back to Home</button></Link>
         </div>
-    );
+        </div>
+     );
 };
 
 export default ErrorPage;
